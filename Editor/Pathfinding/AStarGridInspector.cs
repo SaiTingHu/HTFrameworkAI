08f7d4a1a0ef8d31255298396b9ed2e701759e95
SaiTingHu <987947865@qq.com>
2019-06-24 16:07:52 +0800

    优化：修改一些BUG。

--- Editor/Pathfinding/AStarGridInspector.cs
+++ Editor/Pathfinding/AStarGridInspector.cs
@@ -10,7 +10,7 @@ namespace HT.Framework.AI
     {
         private AStarGrid _target;
 
-        private void OnEnable()
+        protected override void OnEnable()
         {
             _target = target as AStarGrid;
         }
