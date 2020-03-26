d21a441ee44fe95f9efcc723484393371640c4db
SaiTingHu <987947865@qq.com>
2020-03-26 14:51:19 +0800

    优化：修正README。

---
+++ Editor/Utility/EditorGlobalToolsAI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace HT.Framework.AI
+{
+    /// <summary>
+    /// HT.Framework.AI编辑器全局工具
+    /// </summary>
+    public static class EditorGlobalToolsAI
+    {
+        #region 层级视图新建菜单
+        /// <summary>
+        /// 新建A*网格
+        /// </summary>
+        [@MenuItem("GameObject/HTFramework.AI/A* Grid", false, 0)]
+        private static void CreateAStarGrid()
+        {
+            GameObject aStar = new GameObject();
+            aStar.name = "New AStarGrid";
+            aStar.transform.localPosition = Vector3.zero;
+            aStar.transform.localRotation = Quaternion.identity;
+            aStar.transform.localScale = Vector3.one;
+            aStar.AddComponent<AStarGrid>();
+            Selection.activeGameObject = aStar;
+            EditorSceneManager.MarkSceneDirty(aStar.scene);
+        }
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -22,10 +22,10 @@ HTFramework的AI模块，必须依赖于HTFramework主框架使用。
 
 ## 使用方法
 
-- 1.拉取框架到项目中的Assets文件夹下（Assets/HTFramework/）。
+- 1.拉取框架到项目中的Assets文件夹下（Assets/HTFramework/），或以添加子模块的形式。
 
-- 2.将框架根目录下的HTFramework.prefab拖到主场景，并删除主场景其他的东西（除了框架的主要模块，其他任何东西都应该是动态加载的）。
+- 2.在入口场景的层级（Hierarchy）视图点击右键，选择 HTFramework -> Main Environment（创建框架主环境），并删除入口场景其他的东西（除了框架的主要模块，其他任何东西都应该是动态加载的）。
 
-- 3.拉取本模块到项目中的Assets文件夹下（Assets/HTFrameworkAI/）。
+- 3.拉取本模块到项目中的Assets文件夹下（Assets/HTFrameworkAI/），或以添加子模块的形式。
 
-- 4.开始开发。
+- 4.参阅各个模块的帮助文档，开始开发。
